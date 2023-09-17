335292e0b57058ce249e3fc8f1d6ac1b0b17f06c
rabiatul1602 <68725262+rabiatul1602@users.noreply.github.com>
2023-09-17 21:48:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ from streamlit_option_menu import option_menu
 import database as db
 import calendar
 from datetime import datetime
+import streamlit as st
+import os
+
+# Everything is accessible via the st.secrets dict:
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+#st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
 
 # SETTINGS
 incomes = ["Salary", "Blog", "Other income"]
