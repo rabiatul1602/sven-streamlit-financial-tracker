c0460a77493ba95c30bae4df0915e8de7cf8d5b5
rabiatul1602 <qdina01@gmail.com>
2023-09-17 20:36:51 +0800

    initial commit

---
+++ database.py
@@ -0,0 +1,26 @@
+import os
+from deta import Deta
+from dotenv import load_dotenv
+
+# load the environment variables
+load_dotenv(".env")
+DETA_KEY = os.getenv("DETA_KEY")
+
+# initialize with a project key
+deta = Deta(DETA_KEY)
+
+# to connect a database
+db = deta.Base("monthly_reports")
+
+def insert_period(period, incomes, expenses, comment):
+    """Returns the report on a successful creation, otherwise raises an error"""
+    return db.put({"key": period, "incomes": incomes, "expenses": expenses, "comment": comment})
+
+def fetch_all_periods():
+    """Returns a dict of all periods (as drop down list for d.visualization)"""
+    res = db.fetch()
+    return res.items
+
+def get_period(period):
+    """If not found, the function will return None"""
+    return db.get(period)
